2c988f2a77632b32d74e1c2d193bb7169ace8682
digaspirate <diogocosta876@hotmail.com>
2021-12-25 23:28:20 +0000

    finished first UI mockup iteration

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,7 +1,4 @@
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextCharacter;
-import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.*;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
@@ -16,7 +13,7 @@ public class Game {
 
     public Game() {
         try {
-            TerminalSize terminalSize = new TerminalSize(100, 50);
+            TerminalSize terminalSize = new TerminalSize(75, 35);
             DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
             Terminal terminal = terminalFactory.createTerminal();
             screen = new TerminalScreen(terminal);
@@ -32,12 +29,29 @@ public class Game {
     private void draw() throws IOException{
         screen.clear();
         TextGraphics screenGraphics = screen.newTextGraphics();
-        //screenGraphics.setCharacter(5,5,'m');
         screenGraphics.setBackgroundColor(TextColor.Factory.fromString("#3A3A3A"));
-        screenGraphics.fillRectangle(new TerminalPosition(0,0), new TerminalSize(100, 50), ' ');
-        screenGraphics.setCharacter(5,5,'m');
+        screenGraphics.fillRectangle(new TerminalPosition(0,0), new TerminalSize(90, 40), ' ');
+        screenGraphics.setCharacter(2,2,'m');
         screenGraphics.setBackgroundColor(TextColor.Factory.fromString("#000000"));
-        screenGraphics.fillRectangle(new TerminalPosition(5,5), new TerminalSize(50, 42), ' ');
+        screenGraphics.fillRectangle(new TerminalPosition(2,2), new TerminalSize(40, 32), ' ');
+
+        //NEXT PIECE
+        screenGraphics.setBackgroundColor(TextColor.Factory.fromString("#3A3A3A"));
+        screenGraphics.putString(48,3, " _  _ _____  _______   ");
+        screenGraphics.putString(48,4, "| \\| | __\\ \\/ /_   _|");
+        screenGraphics.putString(48,5, "| .` | _| >  <  | | ");
+        screenGraphics.putString(48,6, "|_|\\_|___/_/\\_\\ |_|");
+        screenGraphics.setBackgroundColor(TextColor.Factory.fromString("#000000"));
+        screenGraphics.fillRectangle(new TerminalPosition(48,8), new TerminalSize(20, 10), ' ');
+
+        //SCORE
+        screenGraphics.setBackgroundColor(TextColor.Factory.fromString("#3A3A3A"));
+        screenGraphics.putString(46,20, "  ___  ___ ___  ___ ___");
+        screenGraphics.putString(46,21, " / __|/ __/ _ \\| _ \\ __|");
+        screenGraphics.putString(46,22, " \\__ \\ (_| (_) |   / _|");
+        screenGraphics.putString(46,23, " |___/\\___\\___/|_|_\\___|");
+        screenGraphics.putString(57,26, "0700", SGR.BOLD);
+
         screen.refresh();
     }
 
