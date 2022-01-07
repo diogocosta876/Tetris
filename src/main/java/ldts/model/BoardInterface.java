9ee3cb744b2089ea60447a950d848561d1caebb7
manueljamorim <manueljoaoamorim@gmail.com>
2022-01-07 00:04:23 +0000

    Comments

--- src/main/java/ldts/model/BoardInterface.java
+++ src/main/java/ldts/model/BoardInterface.java
@@ -4,10 +4,9 @@ public interface BoardInterface {
     int getLength();
     int getWidth();
     String[][] getMatrix();
+
     void addPiece(Piece piece);
-
-    boolean canMove(int nextPosX, Piece piece);
-    boolean hasHitBottom(Piece piece);
+    boolean canMove(int nextPosX, Piece piece);//CHECKS IF PIECE ISNT MOVINTO ONTO ANOTHER PIECE
+    boolean hasHitBottom(Piece piece);//CHECKS IF PIECE HAS HIT THE BOTTOM OR A BOTTOM PIECE
     int checkLineCompletition();
-
 }
