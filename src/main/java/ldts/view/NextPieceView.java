16bb5db0182917ab73aeba2d744be57a3fcc82fc
JaySuave <up202004823@up.pt>
2022-01-18 15:24:36 +0000

    NextPieceView INTERFACE

---
+++ src/main/java/ldts/view/NextPieceView.java
@@ -0,0 +1,24 @@
+package ldts.view;
+
+import com.googlecode.lanterna.graphics.TextGraphics;
+import ldts.model.Piece;
+import ldts.model.PieceStates.PieceState;
+
+public class NextPieceView extends View<Piece>{
+    private String[][] matrix;
+    private PieceState state;
+    private int pos_x;
+    private int pos_y;
+
+    public NextPieceView(Piece model) {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void draw(TextGraphics screenGraphics){
+
+    }
+}
