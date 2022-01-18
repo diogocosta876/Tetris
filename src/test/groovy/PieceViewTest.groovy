aa0699f3552a2a2260c751ea61aff0a3b16ee071
JaySuave <up202004823@up.pt>
2022-01-18 15:15:31 +0000

    ScoreView TEST

--- src/test/groovy/PieceViewTest.groovy
+++ src/test/groovy/PieceViewTest.groovy
@@ -1,20 +1,9 @@
-import com.googlecode.lanterna.TerminalSize
-import com.googlecode.lanterna.TextCharacter
-import com.googlecode.lanterna.graphics.TextGraphics
-import com.googlecode.lanterna.screen.TerminalScreen
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory
-import com.googlecode.lanterna.terminal.Terminal
-import javafx.stage.Screen
-import jdk.internal.cmm.SystemResourcePressureImpl
 import ldts.controller.GameController
 import ldts.model.Piece
 import ldts.model.PieceStates.SquarePiece
 import ldts.view.PieceView
 import spock.lang.Specification
 
-import com.googlecode.lanterna.SGR;
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.TerminalSize;
@@ -54,22 +43,22 @@ class PieceViewTest extends Specification{
         def color = screenGraphics.getCharacter(i,j).backgroundColor.red;
 
         then:
-        screenGraphics.getCharacter(i,j).backgroundColor.red==0;
-        screenGraphics.getCharacter(i,j).backgroundColor.green==51;
-        screenGraphics.getCharacter(i,j).backgroundColor.blue==204;
-        screenGraphics.getCharacter(i+1,j).backgroundColor.red==0;
-        screenGraphics.getCharacter(i+1,j).backgroundColor.green==51;
-        screenGraphics.getCharacter(i+1,j).backgroundColor.blue==204;
-        screenGraphics.getCharacter(i+1,j+1).backgroundColor.red==0;
-        screenGraphics.getCharacter(i+1,j+1).backgroundColor.green==51;
-        screenGraphics.getCharacter(i+1,j+1).backgroundColor.blue==204;
-        screenGraphics.getCharacter(i,j+1).backgroundColor.red==0;
-        screenGraphics.getCharacter(i,j+1).backgroundColor.green==51;
-        screenGraphics.getCharacter(i,j+1).backgroundColor.blue==204;
+        screenGraphics.getCharacter(i,j).backgroundColor.red==0
+        screenGraphics.getCharacter(i,j).backgroundColor.green==51
+        screenGraphics.getCharacter(i,j).backgroundColor.blue==204
+        screenGraphics.getCharacter(i+1,j).backgroundColor.red==0
+        screenGraphics.getCharacter(i+1,j).backgroundColor.green==51
+        screenGraphics.getCharacter(i+1,j).backgroundColor.blue==204
+        screenGraphics.getCharacter(i+1,j+1).backgroundColor.red==0
+        screenGraphics.getCharacter(i+1,j+1).backgroundColor.green==51
+        screenGraphics.getCharacter(i+1,j+1).backgroundColor.blue==204
+        screenGraphics.getCharacter(i,j+1).backgroundColor.red==0
+        screenGraphics.getCharacter(i,j+1).backgroundColor.green==51
+        screenGraphics.getCharacter(i,j+1).backgroundColor.blue==204
 
-        screenGraphics.getCharacter(i-1,j).backgroundColor.red==0;
-        screenGraphics.getCharacter(i-1,j).backgroundColor.green==0;
-        screenGraphics.getCharacter(i-1,j).backgroundColor.blue==0;
+        screenGraphics.getCharacter(i-1,j).backgroundColor.red==0
+        screenGraphics.getCharacter(i-1,j).backgroundColor.green==0
+        screenGraphics.getCharacter(i-1,j).backgroundColor.blue==0
 
 
 
---
+++ src/test/groovy/ScoreViewTest.groovy
@@ -0,0 +1,41 @@
+import com.googlecode.lanterna.TerminalSize
+import com.googlecode.lanterna.graphics.TextGraphics
+import com.googlecode.lanterna.screen.Screen
+import com.googlecode.lanterna.screen.TerminalScreen
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory
+import com.googlecode.lanterna.terminal.Terminal
+import ldts.model.Score
+import ldts.view.ScoreView
+import spock.lang.Specification
+
+class ScoreViewTest extends Specification{
+    def 'ScoreView Test'(){
+        given:
+        Score score = new Score()
+
+        Screen screen
+        Terminal terminal
+        try {
+            TerminalSize terminalSize = new TerminalSize(70, 70)
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize)
+            terminal = terminalFactory.createTerminal()
+            screen = new TerminalScreen(terminal)
+            screen.setCursorPosition(null)
+            screen.startScreen()
+            screen.doResizeIfNecessary()
+        } catch (IOException e) {
+            e.printStackTrace()
+        }
+        TextGraphics screenGraphics = screen.newTextGraphics()
+
+        when:
+
+        ScoreView scoreView = new ScoreView(score)
+        scoreView.draw(screenGraphics)
+        screen.refresh()
+
+        then:
+
+        screenGraphics.getCharacter(52,24).character == "0"
+    }
+}
