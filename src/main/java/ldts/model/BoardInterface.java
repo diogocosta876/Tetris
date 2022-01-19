4b5b5e1a377dbdf212def995201a9e559b44dc32
manueljamorim <manueljoaoamorim@gmail.com>
2022-01-19 17:56:06 +0000

    Rotation Collision INTERFACE

--- src/main/java/ldts/model/BoardInterface.java
+++ src/main/java/ldts/model/BoardInterface.java
@@ -9,4 +9,5 @@ public interface BoardInterface {
     boolean canMove(int nextPosX, Piece piece);//CHECKS IF PIECE ISNT MOVINTO ONTO ANOTHER PIECE
     boolean hasHitBottom(Piece piece);//CHECKS IF PIECE HAS HIT THE BOTTOM OR A BOTTOM PIECE
     int checkLineCompletition(RemoveLine remover);
+    boolean canRotate(Piece piece);
 }
