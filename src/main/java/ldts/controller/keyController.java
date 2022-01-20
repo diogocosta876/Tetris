57d145ce255108e609cd1f3cd11d6fb643ad9c7b
JaySuave <up202004823@up.pt>
2022-01-20 17:03:51 +0000

    keyController alterations

--- src/main/java/ldts/controller/keyController.java
+++ src/main/java/ldts/controller/keyController.java
@@ -10,6 +10,10 @@ public class keyController {
     private static volatile boolean upPressed = false;
     private static volatile boolean downPressed = false;
     private static volatile boolean escPressed = false;
+    private static volatile boolean zeroPressed = false;
+    private static volatile boolean onePressed = false;
+    private static volatile boolean twoPressed = false;
+    private static volatile boolean threePressed = false;
 
     public static boolean isLeftPressed() {
         synchronized (keyController.class) {
@@ -36,6 +40,26 @@ public class keyController {
             return escPressed;
         }
     }
+    public static boolean isZeroPressed() {
+        synchronized (keyController.class) {
+            return zeroPressed;
+        }
+    }
+    public static boolean isOnePressed() {
+        synchronized (keyController.class) {
+            return onePressed;
+        }
+    }
+    public static boolean isTwoPressed() {
+        synchronized (keyController.class) {
+            return twoPressed;
+        }
+    }
+    public static boolean isThreePressed() {
+        synchronized (keyController.class) {
+            return threePressed;
+        }
+    }
 
     public static void controller_override() {
         KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
@@ -59,6 +83,18 @@ public class keyController {
                             else if (mykey.getKeyCode() == KeyEvent.VK_ESCAPE) {
                                 escPressed = true;
                             }
+                            else if (mykey.getKeyCode() == KeyEvent.VK_0) {
+                                zeroPressed = true;
+                            }
+                            else if (mykey.getKeyCode() == KeyEvent.VK_1) {
+                                onePressed = true;
+                            }
+                            else if (mykey.getKeyCode() == KeyEvent.VK_2) {
+                                twoPressed = true;
+                            }
+                            else if (mykey.getKeyCode() == KeyEvent.VK_3) {
+                                threePressed = true;
+                            }
                             break;
 
                         case KeyEvent.KEY_RELEASED:
@@ -76,6 +112,18 @@ public class keyController {
                             }
                             else if (mykey.getKeyCode() == KeyEvent.VK_ESCAPE) {
                                 escPressed = false;
+                            }
+                            else if (mykey.getKeyCode() == KeyEvent.VK_0) {
+                                zeroPressed = false;
+                            }
+                            else if (mykey.getKeyCode() == KeyEvent.VK_1) {
+                                onePressed = false;
+                            }
+                            else if (mykey.getKeyCode() == KeyEvent.VK_2) {
+                                twoPressed = false;
+                            }
+                            else if (mykey.getKeyCode() == KeyEvent.VK_3) {
+                                threePressed = false;
                             }
                             break;
                     }
