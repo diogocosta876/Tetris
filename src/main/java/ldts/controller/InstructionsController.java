4883218dc8719f09391e3d38ca89d122597f9f88
JaySuave <up202004823@up.pt>
2022-01-20 16:33:39 +0000

    InstructionsController IMPLEMENTATION

--- src/main/java/ldts/controller/InstructionsController.java
+++ src/main/java/ldts/controller/InstructionsController.java
@@ -21,25 +21,95 @@ public class InstructionsController {
     List<Order> undoOrderQueue = new ArrayList<>();
 
     public InstructionsController(Screen scr, Game gm){
-        
+        screen = scr;
+        game = gm;
+        orderQueue = new ArrayList<>();
+        on = true;
     }
 
-    public void run(){
-
+    public void run() throws IOException {
+        while(on){
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            inputReceiver();
+            try {
+                draw();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
-    public void draw(){
+    public void draw() throws IOException{
+        screen.clear();
 
+        TextGraphics screenGraphics = screen.newTextGraphics();
+        screenGraphics.setBackgroundColor(TextColor.Factory.fromString("#3A3A3A"));
+        screenGraphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(80, 40), ' ');
+
+        screenGraphics.putString(10,3, checkDifficultyChanges());
+
+        screenGraphics.putString(10,10,"0) Increase Difficulty");
+        screenGraphics.putString(10,15,"1) Decrease Difficulty");
+        screenGraphics.putString(10,20,"2) Undo Changes");
+        screenGraphics.putString(10,25,"3) Exit");
+        screen.refresh();
     }
 
-    public void inputReceiver(){
+    public void inputReceiver() throws IOException{
+        if (keyController.isZeroPressed()) {
+            Order order = new HighDifficultyOrder(game);
+            orderQueue.add(order);
+            Order undoOrder = new LowDifficultyOrder(game);
+            undoOrderQueue.add(undoOrder);
+        }
+        else if (keyController.isOnePressed()) {
+            Order order = new LowDifficultyOrder(game);
+            orderQueue.add(order);
+            Order undoOrder = new HighDifficultyOrder(game);
+            undoOrderQueue.add(undoOrder);
+        }
+        if (keyController.isTwoPressed()) {
+            orderQueue.clear();
+            processOrders(undoOrderQueue);
+            undoOrderQueue.clear();
+        }
+        if (keyController.isThreePressed()) {
+            processOrders(orderQueue);
+            on=false;
+        }
+    }
 
+    public String checkDifficultyChanges(){
+        int counter = 0;
+        for(Order order:orderQueue){
+            if(order instanceof HighDifficultyOrder ){
+                counter-=1;
+            }
+            else if(order instanceof LowDifficultyOrder){
+                counter+=1;
+            }
+        }
+        if(counter>0){
+            return "Difficulty will be lower";
+        }
+        else if(counter<0){
+            return "Difficulty will be higher";
+        }
+        else{
+            return "Difficulty will remain the same";
+        }
     }
 
     public void processOrders(List<Order> ordersQueue){
-
+        for(Order order:ordersQueue){
+            order.execute();
+        }
     }
     public void setOn(){
-
+        on = true;
     }
 }
